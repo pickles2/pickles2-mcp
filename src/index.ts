001a1a0fe3ce40122b502ba6408adbf8a1fefb6b
Tomoya Koyanagi <tomk79@gmail.com>
2025-05-03 03:26:28 +0900

    CLIオプション `--debug`, `--entry-script` を追加

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,47 @@
 import { McpServer, ResourceTemplate } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { z } from "zod";
+import { Command } from 'commander';
+
+const version = "0.1.0";
+const program = new Command();
 
 // Create an MCP server
 const server = new McpServer({
-  name: "Pickles 2 MCP",
-  version: "0.1.0"
+	name: "Pickles 2 MCP",
+	version: version,
 });
 
-// Add an addition tool
-server.tool("add",
-  { a: z.number(), b: z.number() },
-  async ({ a, b }) => ({
-    content: [{ type: "text", text: String(a + b) }]
-  })
-);
+program
+	.version(version)
+	.description('A MCP server for Pickles 2')
+	.option('-d, --debug', 'output extra debugging')
+	.requiredOption('--entry-script <path>', '[required] entry script for the Pickles 2.')
+	.action(async (cliOptions) => {
 
-// Add a dynamic greeting resource
-server.resource(
-  "greeting",
-  new ResourceTemplate("greeting://{name}", { list: undefined }),
-  async (uri, { name }) => ({
-    contents: [{
-      uri: uri.href,
-      text: `Hello, ${name}!`
-    }]
-  })
-);
+		// Add an addition tool
+		server.tool("add",
+			{ a: z.number(), b: z.number() },
+			async ({ a, b }) => ({
+				content: [{ type: "text", text: String(a + b) }]
+			})
+		);
 
-// Start receiving messages on stdin and sending messages on stdout
-const transport = new StdioServerTransport();
-await server.connect(transport);
+		// Add a dynamic greeting resource
+		server.resource(
+			"greeting",
+			new ResourceTemplate("greeting://{name}", { list: undefined }),
+			async (uri, { name }) => ({
+				contents: [{
+					uri: uri.href,
+					text: `Hello, ${name}!`
+				}]
+			})
+		);
+
+		// Start receiving messages on stdin and sending messages on stdout
+		const transport = new StdioServerTransport();
+		await server.connect(transport);
+	});
+
+program.parse(process.argv);
